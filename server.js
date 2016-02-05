0779e57bd513f4f621aa6e25f62fa1829b9de40a
Claus Bové <clbo@kea.dk>
2016-02-06 00:38:42 +0100

    class exampes added

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
+var server = http.createServer(function(req, res) {
 
-var server = http.createServer(function(req, res){
-
-	res.writeHead(200, {'Content-Type' : 'text/html'} );
-	res.write('index.html');
-
-
-	res.write('<p>This is from node.js class</p>');
-	res.end();
+    fs.readFile('index.html', 'utf-8', function(err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(data);
+        res.end();
+    });
 
 });
-
-
 server.listen(3000);
